d453e69fc4fbdee4603d4612b924121fcb43bad6
dcaro1996 <dcaro1996@yahoo.com>
2021-12-23 15:31:13 -0500

    introduced a few functions to JS file

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,24 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+
+// Generator Functions
+function getRandomLowercase () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLowercase());
+
+function getRandomUppercase () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUppercase());
+
+function getRandomNumeric () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNumeric());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
